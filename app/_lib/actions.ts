a04fdf1acb52f12ddbc0f81587959233931319af
nikovolaric <niko.volaric@gmail.com>
2025-02-06 14:18:36 +0100

    initial commit

--- app/_lib/actions.ts
+++ app/_lib/actions.ts
@@ -72,7 +72,7 @@ export async function logout() {
 const s3 = new S3Client({
   region: process.env.AWS_BUCKET_REGION!,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY!,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
   },
 });
